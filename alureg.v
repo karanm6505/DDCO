055f3506c00de2e006276b660e8b19c35ed907dd
Karan <karanm6505@gmail.com>
2024-10-01 15:03:02 +0530

    completed

--- alureg.v
+++ alureg.v
@@ -1,45 +1,15 @@
-module alureg (
-    input wire clk,
-    input wire reset,
-    input wire [1:0] op,
-    input wire [2:0] rd_addr_a,
-    input wire [2:0] rd_addr_b,
-    input wire [2:0] wr_addr,
-    input wire wr,
+module alureg (input wire clk,input wire reset,input wire [1:0] op,
+    input wire [2:0] rd_addr_a,input wire [2:0] rd_addr_b,
+    input wire [2:0] wr_addr,input wire wr,
     input wire sel,
-    input wire [15:0] d_in,
-    output wire [15:0] d_out_a,
-    output wire [15:0] d_out_b,
+    input wire [15:0] d_in,output wire [15:0] d_out_a,output wire [15:0] d_out_b,
     output wire [15:0] alu_out,
-    output wire cout
-);
-
+    output wire cout);
     wire [15:0] alu_in_a, alu_in_b;
     wire [15:0] reg_in;
-
-    reg_file reg_file_inst (
-        .clk(clk),
-        .reset(reset),
-        .wr(wr),
-        .rd_addr_a(rd_addr_a),
-        .rd_addr_b(rd_addr_b),
-        .wr_addr(wr_addr),
-        .d_in(reg_in),
-        .d_out_a(alu_in_a),
-        .d_out_b(alu_in_b)
-    );
-
-    alu alu_inst (
-        .op(op),
-        .i0(alu_in_a),
-        .i1(alu_in_b),
-        .o(alu_out),
-        .cout(cout)
-    );
-
+    reg_file uut (.clk(clk),.reset(reset),.wr(wr),.rd_addr_a(rd_addr_a),.rd_addr_b(rd_addr_b),.wr_addr(wr_addr),.d_in(reg_in),.d_out_a(alu_in_a),.d_out_b(alu_in_b));
+    alu uut (.op(op),.i0(alu_in_a),.i1(alu_in_b),.o(alu_out),.cout(cout));
     assign reg_in = sel ? alu_out : d_in;
-
     assign d_out_a = alu_in_a;
     assign d_out_b = alu_in_b;
-
 endmodule
